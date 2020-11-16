24cc6b19a6ae267fb3cc5574c387c477e1e03954
Forge <johnforge1337@gmail.com>
2020-11-16 14:43:39 -0600

    Se cambiaron las graficas

---
+++ Graficas/BarrasA.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once "../conexion2.php";
+$conexion = conexion();
+
+$sql = "SELECT concepto,costo from activo";
+$result = mysqli_query($conexion, $sql);
+
+$valoresY = array();
+$valoresX = array();
+
+while ($ver = mysqli_fetch_row($result)) {
+    $valoresY[] = $ver[1]; //monto
+    $valoresX[] = $ver[0]; //fecha
+}
+
+$datosX = json_encode($valoresX);
+$datosY = json_encode($valoresY);
+?>
+
+<div id="graficaBarras"></div>
+
+<script type="text/javascript">
+    function crearCadenaBarras(json) {
+        var parsed = JSON.parse(json);
+        var arr = [];
+        for (var x in parsed) {
+            arr.push(parsed[x]);
+        }
+        return arr;
+    }
+</script>
+
+<script type="text/javascript">
+    datosX = crearCadenaBarras('<?php echo $datosX; ?>');
+    datosY = crearCadenaBarras('<?php echo $datosY; ?>');
+    var data = [{
+        x: datosX,
+        y: datosY,
+        type: 'bar',
+        marker: {
+            color: 'red'
+        }
+    }];
+
+
+    var layout = {
+        title: 'Grafica de Activos',
+        font: {
+            family: 'Raleway, sans-serif'
+        },
+        xaxis: {
+            tickangle: -45,
+            title: 'fechas'
+        },
+        yaxis: {
+            title: '$$ Montos'
+        },
+        bargap: 0.05
+    };
+
+    Plotly.newPlot('graficaBarras', data, layout);
+</script>
